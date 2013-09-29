787a833317188a1646c1be3821758394799da18a
jagm <jagiela.mateusz@gmail.com>
2013-09-29 20:48:59 +0200

    extract notes to methods

--- js/app.js
+++ js/app.js
@@ -1,5 +1,17 @@
 $(function () {
 
+    var NOTES = {
+        1: 'whole',
+        2: 'half',
+        4: 'quarter',
+        8: 'eighth'
+    };
+    
+    var N1 = function(note) { return NOTES[1] + '|' + note; }
+    var N2 = function(note) { return NOTES[2] + '|' + note; }
+    var N4 = function(note) { return NOTES[4] + '|' + note; }
+    var N8 = function(note) { return NOTES[8] + '|' + note; }
+
     var wlazlKotek = {
         timeSignature: [3, 4],
         tempo: 120,
@@ -11,12 +23,12 @@ $(function () {
         },
         notes: {
             rightHand: [
-                'quarter|G4', 'quarter|E4', 'quarter|E4',
-                'quarter|F4', 'quarter|D4', 'quarter|D4',
-                'eighth|C4', 'eighth|E4', 'half|G4',
-                'quarter|G4', 'quarter|E4', 'quarter|E4',
-                'quarter|F4', 'quarter|D4', 'quarter|D4',
-                'eighth|C4', 'eighth|E4', 'half|C4'
+                N4('G4'), N4('E4'), N4('E4'),
+                N4('F4'), N4('D4'), N4('D4'),
+                N8('C4'), N8('E4'), N2('G4'),
+                N4('G4'), N4('E4'), N4('E4'),
+                N4('F4'), N4('D4'), N4('D4'),
+                N8('C4'), N8('E4'), N2('C4')
             ]
         }
     };
