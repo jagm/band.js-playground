125d05f2797032546e7dfe9696c9a0b9a8d36b8a
jagm <jagiela.mateusz@gmail.com>
2013-09-29 20:16:08 +0200

    change melody to "wlazł kotek"

--- js/app.js
+++ js/app.js
@@ -1,15 +1,34 @@
 $(function() {
 
     var music = new BandJS();
-    music.setTimeSignature(4,4);
+    music.setTimeSignature(3,4);
     music.setTempo(120);
 
     var piano = music.createInstrument();
 
-    piano.note('quarter', 'C4');
-    piano.note('quarter', 'D4');
+    piano.note('quarter', 'G4');
     piano.note('quarter', 'E4');
+    piano.note('quarter', 'E4');
+
     piano.note('quarter', 'F4');
+    piano.note('quarter', 'D4');
+    piano.note('quarter', 'D4');
+
+    piano.note('eighth', 'C4');
+    piano.note('eighth', 'E4');
+    piano.note('half', 'G4');
+
+    piano.note('quarter', 'G4');
+    piano.note('quarter', 'E4');
+    piano.note('quarter', 'E4');
+
+    piano.note('quarter', 'F4');
+    piano.note('quarter', 'D4');
+    piano.note('quarter', 'D4');
+
+    piano.note('eighth', 'C4');
+    piano.note('eighth', 'E4');
+    piano.note('half', 'C4');
 
     piano.finish();
     music.end();
